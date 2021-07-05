0d0ba8d569453a520049fc5e9c4bd208a96d01be
于大帅 <yushuaizb@163.com>
2021-07-05 16:52:53 +0800

    一个测试文件

---
+++ service-feign/src/test/java/com/forezp/servicefeign/Test.java
@@ -0,0 +1,12 @@
+package com.forezp.servicefeign;
+
+/**
+ * @author yudashuai
+ * @date 2021/07/05  16:51
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
